19bc06502b940e079b352c60fb22742f0dea45eb
junkim <junkim@localhost>
2022-05-05 17:06:45 -0400

    Search Test

--- vCreative/src/vPro/SearchTest.java
+++ vCreative/src/vPro/SearchTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -29,5 +30,26 @@ public class SearchTest extends BaseTest {
 		String expected = "Demo Sales Executive";
 		Assert.assertEquals(expected, user);
 		Thread.sleep(4000);
+		
+		d.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@src='./assets/img/icons/search-filter.svg']")));
+		driver.findElement(By.xpath("//img[@src='./assets/img/icons/search-filter.svg']")).click();
+		driver.findElement(By.id("spot_title")).click();
+		driver.findElement(By.id("spot_title")).sendKeys("JUN TEST");
+		driver.findElement(By.id("do-search-btn")).click();
+		Thread.sleep(4000);
+		
+		String text = driver.findElement(By.xpath("(//span[contains(text(),'Title: JUN TEST')])[1]")).getText();
+		String textToCheck = "TITLE: JUN TEST";
+		Assert.assertEquals(textToCheck, text);
+		
+		// Log out
+		d.until(ExpectedConditions.elementToBeClickable(By.xpath("//app-header/div[1]/div[2]/div[4]/span[1]/span[1]/span[2]")));
+		driver.findElement(By.xpath("//app-header/div[1]/div[2]/div[4]/span[1]/span[1]/span[2]")).click();
+		Thread.sleep(2000);
+		d.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Logout')]")));
+		driver.findElement(By.xpath("//span[contains(text(),'Logout')]")).click();
+		System.out.println("PASS Search Test");
+		
+		
 	}
 }
